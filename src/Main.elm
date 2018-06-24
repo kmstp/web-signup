fc555d42ae6fc3f9a0670b93fd91b90d9228f4e8
Truong Hoang Dung <checkraiser11@gmail.com>
2018-06-24 13:17:28 +0700

    Fix

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,141 @@
 module Main exposing (..)
 
-import Html exposing (program)
-import Msgs exposing (Msg)
-import Models exposing (Model)
-import Update exposing (update)
-import View exposing (view)
+import Html exposing (..)
+import Html.Attributes exposing (href, class, style, type_, id)
+import Material
+import Material.Textfield as Textfield exposing(..)
+import Material.Scheme
+import Material.Button as Button
+import Material.Options as Options exposing (css)
+import Material.Layout as Layout
+import Material.Typography as Typo
+import Material.Color as Color
+import WebSocket
+
+-- MODEL
+type alias Mdl =
+    Material.Model
+
+type alias Model =
+    { count : Int
+    , mdl :
+        Material.Model
+        -- Boilerplate: model store for any and all Mdl components you use.
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+model : Model
+model =
+    { count = 0
+    , mdl =
+        Material.model
+        -- Boilerplate: Always use this initial Mdl model store.
+    }
+
+
+
+-- ACTION, UPDATE
+
+
+type Msg
+    = Register
+    | Reset
+    | NewMessage String
+    | NoOp
+    | Mdl (Material.Msg Msg)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Register ->
+            ( model
+            , WebSocket.send "ws://echo.websocket.org" (toString  model.count)
+            )
+
+        Reset ->
+            ( { model | count = 0 }
+            , Cmd.none
+            )
+        NewMessage str ->
+            let _ = Debug.log "New message: " str
+            in
+            (
+              { model | count = model.count - 1}
+            , Cmd.none
+            )
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+
+view : Model -> Html Msg
+view model = Layout.render Mdl model.mdl
+  [ Layout.fixedHeader
+  ]
+  { header = [  ]
+  , drawer = [  ]
+  , tabs = ([ text "Milk", text "Oranges" ], [])
+  , main = [ mainContent model ]
+  }
+
+mainContent : Model -> Html Msg
+mainContent model =
+      div []
+        [ Options.styled p
+          [ Typo.display1 ]
+          [ text "Registration" ]
+        , div []
+            [ div []
+              [ Textfield.render Mdl [1] model.mdl
+                [ Textfield.label "Name"
+                , Textfield.floatingLabel
+                , Textfield.text_
+                ]
+                []
+              ]
+            , div []
+              [ Textfield.render Mdl [2] model.mdl
+                [ Textfield.label "Email"
+                , Textfield.floatingLabel
+                , Textfield.text_
+                ]
+                []
+              ]
+            , div []
+              [ Textfield.render Mdl [3] model.mdl
+                [ Textfield.label "Password"
+                , Textfield.floatingLabel
+                , Textfield.password
+                ]
+                []
+              ]
+            , div []
+              [ Button.render Mdl [4] model.mdl
+                [ Button.raised
+                , Button.ripple
+                , Options.onClick Register
+                ]
+                [ text "Registration"]
+              ]
+            , text (toString model.count)
+            ]
+        ]            
+        |> Material.Scheme.topWithScheme Color.Teal Color.LightBlue
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
-
+  WebSocket.listen "ws://echo.websocket.org" NewMessage
 
 main : Program Never Model Msg
 main =
     Html.program
-        { view = view
-        , init = init
+        { init = ( model, Cmd.none )
+        , view = view
+        , subscriptions = subscriptions
         , update = update
-        , subscriptions = always Sub.none
-        }
+    }
